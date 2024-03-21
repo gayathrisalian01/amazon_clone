7e461eeb6a7f29df786d2091664d1240b7b7d796
Gayathri <gayathripsalian2001@gmail.com>
2024-03-21 17:09:14 +0530

    header and home

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,38 +1,47 @@
 import React from 'react';
 import './Header.css';
-import { FaSearch } from "react-icons/fa";
-import { FaCartShopping } from "react-icons/fa6";
-
-
+import { FaSearch, FaShoppingCart } from "react-icons/fa";
 
 const Header = () => {
   return (
     <header className="header">
-      <div className="header__logo" src="https://www.amazon.com/ref=nav_logo">Amazon</div>
+      <div className="inner-header">
+      <div className="header__logo">
+        <a href="https://www.amazon.com/" className="header__logoLink">
+          Amazon
+        </a>
+      </div>
       
       <div className="header__search">
-        <input className="header__searchInput" type="text" />
+        <input className="header__searchInput" type="text" placeholder="Search" />
         <FaSearch className="header__searchIcon"/>
-        
       </div>
+      
       <div className="header__nav">
-      <div className="header__nav__options">
-        <div className="header__nav__option1">Hello, sign in</div>
-        <div className="header__nav__option2">Accounts & Lists</div>
-      </div>
-
-      <div className="header__nav__options">
-        <div className="header__nav__option1">Returns</div>
-        <div className="header__nav__option2">& Orders</div>
-      </div>
-
+        <div className="header__nav__options">
+          <div className="header__nav__option1">Hello, Sign in</div>
+          <div className="header__nav__option2">Accounts & Lists</div>
+        </div>
+        
+        <div className="header__nav__options">
+          <div className="header__nav__option1">Returns</div>
+          <div className="header__nav__option2">& Orders</div>
+        </div>
+        
         <div className="header__optionBasket">
-        <FaCartShopping />
-            <span className="header__optionLineTwo header__basketCount">
-              Cart
-            </span>
-          </div>
-          </div>
+          <FaShoppingCart />
+          <span className="header__optionLineTwo header__basketCount">
+            Cart
+          </span>
+        </div>
+      </div>
+      </div>
+      <div className="header__nav2">
+        <a href="#" className="header__navLink">Departments</a>
+        <a href="#" className="header__navLink">Today's Deals</a>
+        <a href="#" className="header__navLink">Gift Cards</a>
+        <a href="#" className="header__navLink">Sell</a>
+      </div>
     </header>
   );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,47 +1,51 @@
 import React from "react";
-import { Carousel } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css'; 
+import Carousel from 'react-bootstrap/Carousel'; 
 import "./Home.css";
 
 function Home() {
   return (
-    <div className="home"> {/* Apply the home class here */}
-      <Carousel>
-        <Carousel.Item>
-          <img
-            className="home__image"
-            src="https://m.media-amazon.com/images/I/71VBt4eTg3L._SX3000_.jpg"
-            alt=""
-          />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="home__image"
-            src="https://m.media-amazon.com/images/I/71Ie3JXGfVL._SX3000_.jpg"
-            alt=""
-          />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="home__image"
-            src="https://m.media-amazon.com/images/I/61CiqVTRBEL._SX3000_.jpg"
-            alt=""
-          />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="home__image"
-            src="https://m.media-amazon.com/images/I/81KkrQWEHIL._SX3000_.jpg"
-            alt=""
-          />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="home__image"
-            src="https://m.media-amazon.com/images/I/61zAjw4bqPL._SX3000_.jpg"
-            alt=""
-          />
-        </Carousel.Item>
-      </Carousel>
+    <div className="home">
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="home__image"
+          src="https://m.media-amazon.com/images/I/71VBt4eTg3L._SX3000_.jpg"
+          alt=""
+        />
+      </Carousel.Item>
+	<Carousel.Item>
+        <img
+          className="home__image"
+          src="https://m.media-amazon.com/images/I/71Ie3JXGfVL._SX3000_.jpg"
+          alt=""
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="home__image"
+          src="https://m.media-amazon.com/images/I/61CiqVTRBEL._SX3000_.jpg"
+          alt=""
+        />
+      </Carousel.Item>
+	<Carousel.Item>
+        <img
+          className="home__image"
+          src="https://m.media-amazon.com/images/I/81KkrQWEHIL._SX3000_.jpg"
+          alt=""
+        />
+      </Carousel.Item>
+	<Carousel.Item>
+        <img
+          className="home__image"
+          src="https://m.media-amazon.com/images/I/61zAjw4bqPL._SX3000_.jpg"
+          alt=""
+        />
+      </Carousel.Item>
+    </Carousel>
+    <div class="textlinkstrip">
+ <p> You are on amazon.com. You can also shop on Amazon India for millions of products with fast local delivery. Click <a href="https://www.amazon.in">here</a> to go to amazon.in</p>
+</div>
     </div>
   );
 };
