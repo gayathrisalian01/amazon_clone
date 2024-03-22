23361f9153955a8bc8d1ffce693e6bf76c14a47e
Gayathri <gayathripsalian2001@gmail.com>
2024-03-22 17:06:12 +0530

    updated home added cards

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,13 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.css'; 
 import Carousel from 'react-bootstrap/Carousel'; 
 import "./Home.css";
+import BasicExample from "./card";
 
 function Home() {
   return (
     <div className="home">
     <Carousel>
-      <Carousel.Item>
+    <Carousel.Item>
         <img
           className="home__image"
           src="https://m.media-amazon.com/images/I/71VBt4eTg3L._SX3000_.jpg"
@@ -44,8 +45,52 @@ function Home() {
       </Carousel.Item>
     </Carousel>
     <div class="textlinkstrip">
- <p> You are on amazon.com. You can also shop on Amazon India for millions of products with fast local delivery. Click <a href="https://www.amazon.in">here</a> to go to amazon.in</p>
+ <p> You are on amazon.com. You can also shop on Amazon India for millions of products with fast local delivery. <a href="https://www.amazon.in">Click here to go to amazon.in</a> </p>
 </div>
+<BasicExample></BasicExample>
+<div className="carousel-container">
+<Carousel>
+  
+        <img
+          className="book__image"
+          src="https://m.media-amazon.com/images/I/81VP1+CNyWL._AC_SY200_.jpg"
+          alt=""
+        />
+    
+        <img
+          className="book__image"
+          src="https://m.media-amazon.com/images/I/81TmnPZWb0L._AC_SY200_.jpg"
+          alt=""
+        />
+    
+  
+        <img
+          className="book__image"
+          src="https://m.media-amazon.com/images/I/8136u75+kkL._AC_SY200_.jpg"
+          alt=""
+        />
+    
+        <img
+          className="book__image"
+          src="https://m.media-amazon.com/images/I/81AHTyq2wVL._AC_SY200_.jpg"
+          alt=""
+        />
+    
+        <img
+          className="book__image"
+          src="https://m.media-amazon.com/images/I/81EmtXw0mrL._AC_SY200_.jpg"
+          alt=""
+        />
+    
+  
+        <img
+          className="book__image"
+          src="https://m.media-amazon.com/images/I/71x3UZ18tmL._AC_SY200_.jpg"
+          alt=""
+        />
+    
+    </Carousel>
+    </div>
     </div>
   );
 };
---
+++ src/components/card.js
@@ -0,0 +1,170 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import "./card.css";
+
+function GamingAccessoriesCard() {
+  return (
+    <div className='cards-row'>
+        <div className='div1'>
+    <Card className="gaming-card">
+      <Card.Body>
+        <Card.Title>Gaming Accessories</Card.Title>
+        <div className="gaming-images">
+          <div className="gaming-item">
+            <Card.Img className='images' variant="top" src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/June/Fuji_Quad_Headset_1x._SY116_CB667159060_.jpg" />
+            <Card.Text className='texts'>Headsets</Card.Text>
+          </div>
+          <div className="gaming-item">
+            <Card.Img className='images' variant="top" src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/June/Fuji_Quad_Keyboard_1x._SY116_CB667159063_.jpg" />
+            <Card.Text className='texts'>Keyboards</Card.Text>
+          </div>
+          <div className="gaming-item">
+            <Card.Img className='images' variant="top" src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/June/Fuji_Quad_Mouse_1x._SY116_CB667159063_.jpg" />
+            <Card.Text className='texts'>Computer mice</Card.Text>
+          </div>
+          <div className="gaming-item">
+            <Card.Img className='images' variant="top" src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/June/Fuji_Quad_Chair_1x._SY116_CB667159060_.jpg" />
+            <Card.Text className='texts'>Chairs</Card.Text>
+          </div>
+        </div>
+        <a href='#' className="see-more">See more</a>
+      </Card.Body>
+    </Card>
+    <Card className="gaming-card">
+      <Card.Body>
+        <Card.Title>Deals in PCs</Card.Title>
+        <div className="pc-images">
+          
+            <Card.Img className='pc-image' variant="top" src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2023/EBF23/Fuji_Desktop_Single_image_EBF_1x_v1._SY304_CB573698005_.jpg" />
+          
+        </div>
+        <a href='#' className="see-more">Shop now</a>
+      </Card.Body>
+    </Card>
+    <Card className="gaming-card">
+      <Card.Body>
+        <Card.Title>Refresh your space</Card.Title>
+        <div className="gaming-images">
+          <div className="gaming-item">
+            <Card.Img className='images-card3' variant="top" src="https://images-na.ssl-images-amazon.com/images/G/01/launchpad/2023/Gateway/January/DesktopQuadCat_186x116_LP-HP_B08MYX5Q2W_01.23._SY116_CB619238939_.jpg" />
+            <Card.Text className='texts'>Dining</Card.Text>
+          </div>
+          <div className="gaming-item">
+            <Card.Img className='images-card3' variant="top" src="https://images-na.ssl-images-amazon.com/images/G/01/launchpad/2023/Gateway/January/DesktopQuadCat_186x116_home_B08RCCP3HV_01.23._SY116_CB619238939_.jpg" />
+            <Card.Text className='texts'>Home</Card.Text>
+          </div>
+          <div className="gaming-item">
+            <Card.Img className='images-card3' variant="top" src="https://images-na.ssl-images-amazon.com/images/G/01/launchpad/2023/Gateway/January/DesktopQuadCat_186x116_kitchen_B0126LMDFK_01.23._SY116_CB619238939_.jpg" />
+            <Card.Text className='texts'>Kitchen</Card.Text>
+          </div>
+          <div className="gaming-item">
+            <Card.Img className='images-card3' variant="top" src="https://images-na.ssl-images-amazon.com/images/G/01/launchpad/2023/Gateway/January/DesktopQuadCat_186x116_health-beauty_B07662GN57_01.23._SY116_CB619238939_.jpg" />
+            <Card.Text className='texts'>Health and beauty</Card.Text>
+          </div>
+        </div>
+        <a href='#' className="see-more">See more</a>
+      </Card.Body>
+    </Card>
+    <Card className="gaming-card">
+      <Card.Body>
+        <Card.Title>Amazon Gadget Store</Card.Title>
+        <div className="gaming-images">
+          <div className="gaming-item">
+            <Card.Img className='images-card3' variant="top" src="https://m.media-amazon.com/images/I/41aF1Kpc1iL._SY160_.jpg" />
+            <Card.Text className='texts'>Smartphones</Card.Text>
+          </div>
+          <div className="gaming-item">
+            <Card.Img className='images-card3' variant="top" src="https://m.media-amazon.com/images/I/316r7290SLL._SY75_.jpg" />
+            <Card.Text className='texts'>Tablets</Card.Text>
+          </div>
+          <div className="gaming-item">
+            <Card.Img className='images-card3' variant="top" src="https://m.media-amazon.com/images/I/31qcGgiIK7L._SY75_.jpg" />
+            <Card.Text className='texts'>Laptops</Card.Text>
+          </div>
+          <div className="gaming-item">
+            <Card.Img className='images-card3' variant="top" src="https://m.media-amazon.com/images/I/313zteqI9LL._SY75_.jpg" />
+            <Card.Text className='texts'>TVs</Card.Text>
+          </div>
+        </div>
+        <a href='#' className="see-more">See more from Amazon Gadgets</a>
+      </Card.Body>
+    </Card>
+    </div>
+    
+    <div className='div2'>
+    <Card className="gaming-card">
+    <Card.Body>
+      <Card.Title>Handpicked music & audio</Card.Title>
+      <div className="gaming-images">
+        <div className="gaming-item">
+          <Card.Img className='images-card3' variant="top" src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2022/img/Consumer_Electronics/XCM_CUTTLE_1468425_2580535_186x116_1X_en_US._SY116_CB609480017_.jpg" />
+          <Card.Text className='texts'>Headphones</Card.Text>
+        </div>
+        <div className="gaming-item">
+          <Card.Img className='images-card3' variant="top" src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2023/img/Consumer_Electronics/XCM_CUTTLE_1546380_2938463_186x116_1X_en_US._SY116_CB613156637_.jpg" />
+          <Card.Text className='texts'>Guitars</Card.Text>
+        </div>
+        <div className="gaming-item">
+          <Card.Img className='images-card3' variant="top" src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2023/img/Consumer_Electronics/XCM_CUTTLE_1545875_2934703_186x116_1X_en_US._SY116_CB613346538_.jpg" />
+          <Card.Text className='texts'>Keyboards</Card.Text>
+        </div>
+        <div className="gaming-item">
+          <Card.Img className='images-card3' variant="top" src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2022/img/Consumer_Electronics/XCM_CUTTLE_1468425_2580538_186x116_1X_en_US._SY116_CB609480017_.jpg" />
+          <Card.Text className='texts'>Microphones</Card.Text>
+        </div>
+      </div>
+      <a href='#' className="see-more">See more curated tech</a>
+    </Card.Body>
+  </Card>
+  <Card className="gaming-card">
+      <Card.Body>
+        <Card.Title>Fill your Easter basket with joy</Card.Title>
+        <div className="pc-images">
+          
+            <Card.Img className='pc-image' variant="top" src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2024/Easter/Fuji_Easter_Dash_card_1x_EN._SY304_CB581341381_.jpg" />
+          
+        </div>
+        <a href='#' className="Shop gifts">See more</a>
+      </Card.Body>
+    </Card>
+    <Card className="gaming-card">
+      <Card.Body>
+        <Card.Title>Top Deal</Card.Title>
+        <div className="pc-images">
+            <Card.Img className='pc-image2' variant="top" src="https://m.media-amazon.com/images/I/31OiAm-FA8L._AC_SY230_.jpg" />
+            <Card.Text className='texts'><span style={{backgroundColor:"red", borderRadius:"2px", padding:"4px",color:"white"}}>Up to 50% off</span><span style={{color:"red"}}>   Limited time deal</span></Card.Text>
+            <Card.Text className='texts'>COSRX Skin Care</Card.Text>          
+        </div>
+        <a href='#' className="See all deals">See more</a>
+      </Card.Body>
+    </Card>
+    <Card className="gaming-card">
+    <Card.Body>
+      <Card.Title>Shop deals in Fashion</Card.Title>
+      <div className="gaming-images">
+        <div className="gaming-item">
+          <Card.Img className='images-card3' variant="top" src="https://images-na.ssl-images-amazon.com/images/G/01/AMAZON_FASHION/2022/SITE_FLIPS/SPR_22/GW/DQC/DQC_APR_TBYB_W_BOTTOMS_1x._SY116_CB624172947_.jpg" />
+          <Card.Text className='texts'>Jeans under $50</Card.Text>
+        </div>
+        <div className="gaming-item">
+          <Card.Img className='images-card3' variant="top" src="https://images-na.ssl-images-amazon.com/images/G/01/AMAZON_FASHION/2022/SITE_FLIPS/SPR_22/GW/DQC/DQC_APR_TBYB_W_TOPS_1x._SY116_CB623353881_.jpg" />
+          <Card.Text className='texts'>Tops under $25</Card.Text>
+        </div>
+        <div className="gaming-item">
+          <Card.Img className='images-card3' variant="top" src="https://images-na.ssl-images-amazon.com/images/G/01/AMAZON_FASHION/2022/SITE_FLIPS/SPR_22/GW/DQC/DQC_APR_TBYB_W_DRESSES_1x._SY116_CB623353881_.jpg" />
+          <Card.Text className='texts'>Dresses under $30</Card.Text>
+        </div>
+        <div className="gaming-item">
+          <Card.Img className='images-card3' variant="top" src="https://images-na.ssl-images-amazon.com/images/G/01/AMAZON_FASHION/2022/SITE_FLIPS/SPR_22/GW/DQC/DQC_APR_TBYB_W_SHOES_1x._SY116_CB624172947_.jpg" />
+          <Card.Text className='texts'>Shoes under $50</Card.Text>
+        </div>
+      </div>
+      <a href='#' className="See all deals">See more curated tech</a>
+    </Card.Body>
+  </Card>
+  </div>
+  </div>
+  );
+}
+
+export default GamingAccessoriesCard;
